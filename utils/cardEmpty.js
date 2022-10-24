59e82c7a07e5cf908c14b276e36325215aa0d4f5
dryxoxo <ibrohimhuzaimi@gmail.com>
2022-10-24 21:06:43 +0700

    fix size

--- utils/cardEmpty.js
+++ utils/cardEmpty.js
@@ -2,11 +2,13 @@ export function emptyCard(title, lokasi, urlImage, id){
     return `
     <div class="col-lg-3 mb-4">
           <div class="card mx-auto h-100" style="width: 18rem;">
-            <img src="${urlImage}" class="card-img-top" alt="${title}">
+            <img src="${urlImage}" class="card-img-top" alt="${title}" style="width: auto; height: 250px;">
             <div class="card-body">
               <h5 class="card-title">${title}</h5>
               <p class="card-text">${lokasi}</p>
-              <a href="kampus/?id=${id}" class="btn btn-primary" id="${id}">Kunjungi</a>
+            </div>
+            <div class="card-footer">
+                <a href="kampus/?id=${id}" class="btn btn-primary" id="${id}">Kunjungi</a>
             </div>
           </div>
         </div>
